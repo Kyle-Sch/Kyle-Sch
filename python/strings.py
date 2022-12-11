05f510c9b6be8dcf6b6255dbc6775c02850fb67f
Kyle Schneiderlochner <39798843+Kyle-Sch@users.noreply.github.com>
2022-12-11 14:19:13 -0500

    add strings python script

---
+++ python/strings.py
@@ -0,0 +1,23 @@
+name = "kyle david"
+# change case methods
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+# Format strings
+firstName = "kyle"
+lastName = "schneiderlochner"
+fullName = f"{firstName} {lastName}"
+print(fullName)
+print(f"Hello {fullName.title()}!")
+
+# \t tab \n next line 
+print("\t Python")
+print("Python\nis \nfun!")
+
+# Whitespace
+language = " Java "
+print(language)
+print(language.rstrip())
+print(language.lstrip())
+print(language.strip())
