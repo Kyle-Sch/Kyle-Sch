62f387b8be425f79dd34f1b221fb74708620d831
Kyle Schneiderlochner <39798843+Kyle-Sch@users.noreply.github.com>
2022-12-11 14:16:40 -0500

    add lists scripts

---
+++ python/lists.py
@@ -0,0 +1,49 @@
+bicyclyes = ["trek", "cannondale", "redline", "specialized"]
+print(bicyclyes)
+print(bicyclyes[0])
+print(bicyclyes[0].title())
+
+message = f"My first bike was a {bicyclyes[1].title()}."
+print(message)
+
+motorcyles = ['honda', 'yamaha', 'suzuki']
+print(motorcyles)
+motorcyles[0] = 'ducati'
+print(motorcyles)
+
+motorcyles.append('davidson')
+print(motorcyles)
+
+motorcyles.insert(0, 'harley')
+print(motorcyles)
+
+del motorcyles[0]
+print(motorcyles)
+
+lastMotorcyle = motorcyles.pop()
+print(lastMotorcyle)
+print(motorcyles)
+
+firstMotorcyle = motorcyles.pop(0)
+print(firstMotorcyle)
+print(motorcyles)
+
+motorcyles.remove("honda")
+print(motorcyles)
+
+# Sorting
+motorcyles.sort() #Perm
+print(motorcyles)
+
+motorcyles.sort(reverse=True)
+print(motorcyles)
+
+print(sorted(motorcyles)) # Temp
+
+motorcyles.reverse()
+print(motorcyles)
+
+len(motorcyles) # length
+
+print(motorcyles[-1]) # last value
+
